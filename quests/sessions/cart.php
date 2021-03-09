1fe3d2ad5a2578c8eb690e0bd8efd0a4fd95e292
a-dagnicourt <dagnicourt.@gmail.com>
2021-03-09 15:58:10 +0100

    added cookies factory template + login/logout feature

---
+++ quests/sessions/cart.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+?>
+
+<?php require 'inc/head.php'; ?>
+<section class="cookies container-fluid">
+    <div class="row">
+        TODO : Display shopping cart items from $_SESSION here.
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
